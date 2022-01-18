e7113ddc752800bb3686e2f461f294ea7b4edf0c
GitHub Action <action@github.com>
2022-01-18 13:20:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/omegaui/intellijvibencode/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/omegaui/intellijvibencode/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.omegaui.intellijvibencode.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.omegaui.intellijvibencode.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/omegaui/intellijvibencode/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/omegaui/intellijvibencode/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.omegaui.intellijvibencode.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.omegaui.intellijvibencode.MyBundle
 
 class MyApplicationService {
 
